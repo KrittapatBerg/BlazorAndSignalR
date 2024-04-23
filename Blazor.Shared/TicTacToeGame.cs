c361f2fd2fc7f6c8115f1d3d3d94c15f50a84b89
KrittapatBerg <99590760+KrittapatBerg@users.noreply.github.com>
2024-04-23 11:53:52 +0200

    Add project files.

---
+++ Blazor.Shared/TicTacToeGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAndSignalR.Shared;
+
+public class TicTacToeGame
+{
+    public string? PlayerXId { get; set; }
+    public string? PlayerOId { get; set; }
+    public string? CurrentPlayerId { get; set; }
+    public string? CurrentPlayetSymbol => CurrentPlayerId == PlayerXId ? "X" : "O";
+    public bool GameStarted { get; set; } = false; /*=> PlayerXId != null && PlayerOId != null;*/
+    public bool GameOver { get; set; }
+    public bool IsDraw { get; set; }
+    public string? Winner { get; set; } = string.Empty;
+    public List<List<string>> Board { get; set; } = new List<List<string>>(3);
+
+    public TicTacToeGame()
+    {
+        InitializeBoard();
+    }
+    private void InitializeBoard()
+    {
+        //Board.Clear();
+        //for (int i = 0; i < 3; i++)
+        //{
+        //    Board.Add(new List<string>(3));
+        //    for (int j = 0; j < 3; j++)
+        //    {
+        //        Board[i].Add(string.Empty);
+        //    }
+        //} this one works as well.  
+
+        Board.Clear();
+        for (int i = 0; i < 3; i++)
+        {
+            var row = new List<string>(3);
+            for (int j = 0; j < 3; j++)
+            {
+                row.Add(string.Empty);
+            }
+            Board.Add(row);
+        }
+    }   
+}
---
+++ BlazorAndSignalR/Hubs/GameHub.cs
@@ -0,0 +1,50 @@
+﻿using Blazor.Shared;
+using BlazorAndSignalR.Shared;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorAndSignalR.Hubs
+{
+    public class GameHub : Hub
+    {
+        private static readonly List<GameRoom> rooms = new(); 
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Player with Id '{Context.ConnectionId}' connected");
+            
+            await Clients.Caller.SendAsync("Rooms", rooms.OrderBy(r => r.RoomName));
+        }
+
+        public async Task<GameRoom> CreateRoom(string name, string playerName)
+        {
+            var roomId = Guid.NewGuid().ToString();
+            GameRoom room = new GameRoom(roomId, name);
+
+            rooms.Add(room);
+
+            var newPlayer = new Player(Context.ConnectionId, playerName);
+            room.TryAddPlayer(newPlayer);
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+            await Clients.All.SendAsync("Rooms", rooms.OrderBy(r => r.RoomName));
+
+            return room;
+        }
+
+        public async Task<GameRoom?> JoinRoom(string roomId, string playerName)
+        {
+            var room = rooms.FirstOrDefault(r => r.RoomId == roomId);
+            if (room is not null)
+            {
+                var newPlayer = new Player(Context.ConnectionId, playerName);
+                if (room.TryAddPlayer(newPlayer))
+                {
+                    await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+                    await Clients.Group(roomId).SendAsync("PlayerJoined", newPlayer);
+                    
+                    return room;
+                }
+            }
+            return null;
+        }
+    }
+}
